6a9018fde2771202ce53c2579e26eecbb6f0d39c
Anthony Bautista <anthonya.bautista@gmail.com>
2024-01-25 16:58:19 -0600

    Add logo and setup for LORE verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,7 @@ module.exports = {
   etherscan: {
     apiKey: {
       snowtrace: "snowtrace", // apiKey is not required, just set a placeholder
+      lore: "lore-public"
     },
     customChains: [
       {
@@ -25,11 +26,23 @@ module.exports = {
           apiURL: "https://api.routescan.io/v2/network/mainnet/evm/43114/etherscan",
           browserURL: "https://avalanche.routescan.io"
         }
+      },
+      {
+        network: "lore",
+        chainId: 43114,
+        urls: {
+          apiURL: " https://api.lorescan.com/43114",
+          browserURL: "https://skilift.io/"
+        }
       }
     ]
   },
   networks: {
     snowtrace: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    lore: {
       url: 'https://api.avax.network/ext/bc/C/rpc',
       accounts: [process.env.PRIVATE_KEY]
     },
